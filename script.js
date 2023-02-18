2675cd2ec2aae9eb9a3f6215bfa2d8237f45b081
ashaqragit <109609168+ashaqragit@users.noreply.github.com>
2023-02-18 13:36:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let playerChoice = "";
 let computerChoice = "";
+let playerScore = 0;
+let computerScore = 0;
+let tieScore = 0;
+let maximumrounds = 5;
+let currentRound = 0;
 
 function generateComputerChoice() {
   let randNumber;
@@ -16,12 +21,110 @@ function generateComputerChoice() {
 function setPlayerRock() {
   playerChoice = "rock";
   console.log("player choice is Rock");
+  generateComputerChoice();
+  playGame();
+  whoWins();
 }
 function setPlayerPaper() {
   playerChoice = "paper";
   console.log("player choice is paper");
+  generateComputerChoice();
+  playGame();
+  whoWins();
 }
 function setPlayerScissors() {
-  playerChoice = "rock";
+  playerChoice = "scissors";
   console.log("player choice is scissors");
+  generateComputerChoice();
+  playGame();
+  whoWins();
+}
+
+function whoWins() {
+  if (currentRound === 5) {
+    if (playerScore > computerScore) {
+      console.log("computer score = " + computerScore);
+      console.log("player score = " + playerScore);
+      console.log("player winnnnnnnnnnnnnnnnnnnnnnn");
+    } else if (playerScore < computerScore) {
+      console.log("computer score = " + computerScore);
+      console.log("player score = " + playerScore);
+      console.log("computer winnnnnnnnnnnnnnnnnnnnnnn");
+    }
+  } else {
+    if (playerScore > computerScore && playerScore - computerScore === 2) {
+      console.log("computer score = " + computerScore);
+      console.log("player score = " + playerScore);
+      console.log("player winnnnnnnnnnnnnnnnnnnnnnn");
+    } else if (
+      playerScore < computerScore &&
+      computerScore - playerScore === 2
+    ) {
+      console.log("computer score = " + computerScore);
+      console.log("player score = " + playerScore);
+      console.log("computer winnnnnnnnnnnnnnnnnnnnnnn");
+    }
+  }
+}
+function updateScores() {
+  console.log("computer score = " + computerScore);
+  console.log("player score = " + playerScore);
+}
+
+function playGame() {
+  if (playerChoice === "rock") {
+    if (computerChoice === "rock") {
+      tieScore++;
+      console.log(
+        `Player choice is ${playerChoice} Computer choice is ${computerChoice} it is a tie  `
+      );
+    } else if (computerChoice === "paper") {
+      computerScore++;
+      console.log(
+        `Player choice is ${playerChoice} Computer choice is ${computerChoice} Computer wins!!!  `
+      );
+    } else if (computerChoice === "scissors") {
+      playerScore++;
+      console.log(
+        `Player choice is ${playerChoice} Computer choice is ${computerChoice} Player wins!!!  `
+      );
+    }
+  } else if (playerChoice === "paper") {
+    if (computerChoice === "paper") {
+      tieScore++;
+      console.log(
+        `Player choice is ${playerChoice} Computer choice is ${computerChoice} it is a tie  `
+      );
+    } else if (computerChoice === "scissors") {
+      computerScore++;
+      console.log(
+        `Player choice is ${playerChoice} Computer choice is ${computerChoice} Computer wins!!!  `
+      );
+    } else if (computerChoice === "rock") {
+      playerScore++;
+      console.log(
+        `Player choice is ${playerChoice} Computer choice is ${computerChoice} Player wins!!!  `
+      );
+    }
+  } else {
+    if (computerChoice === "scissors") {
+      tieScore++;
+      console.log(
+        `Player choice is ${playerChoice} Computer choice is ${computerChoice} it is a tie  `
+      );
+    } else if (computerChoice === "rock") {
+      computerScore++;
+      console.log(
+        `Player choice is ${playerChoice} Computer choice is ${computerChoice} Computer wins!!!  `
+      );
+    } else if (computerChoice === "paper") {
+      playerScore++;
+      console.log(
+        `Player choice is ${playerChoice} Computer choice is ${computerChoice} Player wins!!!  `
+      );
+    }
+  }
+  updateScores();
+  currentRound++;
+  console.log("current Round " + currentRound);
 }
